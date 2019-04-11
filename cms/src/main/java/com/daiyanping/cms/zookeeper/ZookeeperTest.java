7a2b39e823be3adc411c423addb4887fb7fbedb1
daiyanping <841943896@qq.com>
2019-04-11 23:17:14 +0800

    zookeeper学习

---
+++ cms/src/main/java/com/daiyanping/cms/zookeeper/ZookeeperTest.java
@@ -0,0 +1,12 @@
+package com.daiyanping.cms.zookeeper;
+
+import org.I0Itec.zkclient.ZkClient;
+
+public class ZookeeperTest {
+
+	public static void main(String[] args) {
+		ZkClient zkClient = new ZkClient("127.0.0.1:2181");
+		zkClient.createEphemeral("/test");
+		Thread.yield();
+	}
+}
