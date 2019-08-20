3d000af57badf433161868c1d8ef6c09ae59ccd3
ypdaic <841943896@qq.com>
2019-08-20 19:09:28 +0800

    zookeeper学习

---
+++ cms/src/main/java/com/daiyanping/cms/zookeeper/ZookeeperNativeApi.java
@@ -0,0 +1,138 @@
+package com.daiyanping.cms.zookeeper;
+
+import lombok.Data;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.concurrent.CountDownLatch;
+
+import static org.apache.zookeeper.CreateMode.EPHEMERAL;
+import static org.apache.zookeeper.CreateMode.EPHEMERAL_SEQUENTIAL;
+
+/**
+ * @ClassName ZookeeperNativeApi
+ * @Description TODO
+ * @Author daiyanping
+ * @Date 2019-08-20
+ * @Version 0.1
+ */
+@Data
+public class ZookeeperNativeApi implements Watcher {
+
+    private static String SERVER_IP_PORT = "192.168.0.4:2181";
+
+    CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws Exception {
+        test1();
+    }
+
+    public static void test1 () throws Exception {
+        ZookeeperNativeApi zookeeperNativeApi = new ZookeeperNativeApi();
+
+        ZooKeeper zooKeeper = new ZooKeeper(SERVER_IP_PORT, 3000, zookeeperNativeApi);
+        zookeeperNativeApi.countDownLatch.await();
+        zookeeperNativeApi.setZooKeeper(zooKeeper);
+
+        // 需要事先添加exits事件，否则不能监听到节点创建事件
+        zooKeeper.exists("/test", true);
+        // 创建一个临时anyone权限的节点(临时节点不能创建子节点)
+        String s = zooKeeper.create("/test", "haha".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, EPHEMERAL);
+        System.out.println(s);
+
+        // 获取节点数据
+        byte[] data = zooKeeper.getData("/test", true, null);
+        System.out.println(new String(data));
+
+        // 变更节点数据，版本给-1表示使用最新的数据进行变更
+        zooKeeper.setData("/test", "改变节点数据".getBytes(), -1);
+
+        // 变更节点数据，版本给-1表示使用最新的数据进行变更
+        zooKeeper.setData("/test", "改变节点数据2".getBytes(), -1);
+
+
+        // 如果需要创建名称相同的节点，节点类型需要是有序的,注意有序节点没法监控
+        String nodePath = zooKeeper.create("/test", "hahadd".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, EPHEMERAL_SEQUENTIAL);
+        System.out.println(nodePath);
+        byte[] data1 = zooKeeper.getData(nodePath, null, null);
+        System.out.println(new String(data1));
+
+        // 删除节点
+        zooKeeper.delete(nodePath, -1, new AsyncCallback.VoidCallback() {
+            @Override
+            public void processResult(int rc, String path, Object ctx) {
+                // 成功 rc则是0，错误则是相应的错误码
+                System.out.println(rc);
+                System.out.println(path);
+                System.out.println(ctx);
+            }
+        }, "自己的上下文");
+
+        // 判断节点是否存在
+        zooKeeper.exists("/test", zookeeperNativeApi, new AsyncCallback.StatCallback() {
+            @Override
+            public void processResult(int rc, String path, Object ctx, Stat stat) {
+                System.out.println(rc);
+                System.out.println(path);
+                System.out.println(ctx);
+                System.out.println(stat);
+            }
+        }, "自己的上下文");
+        zooKeeper.close();
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        try {
+
+            Event.KeeperState state = event.getState();
+            Event.EventType type = event.getType();
+            if (state.getIntValue() == Event.KeeperState.SyncConnected.getIntValue()) {
+
+                if (type.getIntValue() == Event.EventType.None.getIntValue()) {
+                    this.countDownLatch.countDown();
+                    System.out.println("连接事件");
+                }
+
+                if (type.getIntValue() == Event.EventType.NodeCreated.getIntValue()) {
+                    String path = event.getPath();
+                    System.out.println(path + ":创建节点事件");
+                    // 再次添加exits事件，否则不能监听到节点创建事件
+                    zooKeeper.exists(path, true);
+            }
+
+                if (type.getIntValue() == Event.EventType.NodeDataChanged.getIntValue()) {
+                    String path = event.getPath();
+                    System.out.println(path + ":节点数据变更事件");
+                    // 由于watch机制是一次性的，在搜到节点变更事件后，需要重新注册数据变更事件
+                    byte[] data = zooKeeper.getData(path, true, null);
+                    System.out.println("变更后的数据:" + new String(data));
+                }
+
+                if (type.getIntValue() == Event.EventType.NodeChildrenChanged.getIntValue()) {
+                    String path = event.getPath();
+                    System.out.println(path + ":子节点变更事件");
+                    zooKeeper.getChildren(path, true);
+                }
+
+                if (type.getIntValue() == Event.EventType.NodeDeleted.getIntValue()) {
+                    String path = event.getPath();
+                    System.out.println("节点删除事件");
+                    // 再次添加exits事件，否则不能监听到节点创建事件
+                    zooKeeper.exists(path, true);
+                }
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+    }
+}
---
+++ cms/src/main/java/com/daiyanping/cms/zookeeper/ZookeeperNativeApiForAcl.java
@@ -0,0 +1,158 @@
+package com.daiyanping.cms.zookeeper;
+
+import lombok.Data;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Id;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
+import static org.apache.zookeeper.CreateMode.EPHEMERAL;
+
+/**
+ * @ClassName ZookeeperNativeApi
+ * @Description TODO
+ * @Author daiyanping
+ * @Date 2019-08-20
+ * @Version 0.1
+ */
+@Data
+public class ZookeeperNativeApiForAcl implements Watcher {
+
+    private static String SERVER_IP_PORT = "192.168.0.4:2181";
+
+    CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    ZooKeeper zooKeeper;
+
+    public static void main(String[] args) throws Exception {
+        test1();
+    }
+
+    public static void test1 () throws Exception {
+        ZookeeperNativeApiForAcl zookeeperNativeApi = new ZookeeperNativeApiForAcl();
+
+        ZooKeeper zooKeeper = new ZooKeeper(SERVER_IP_PORT, 3000, zookeeperNativeApi);
+
+        // 添加用户
+        zooKeeper.addAuthInfo("digest", "123456".getBytes());
+        zookeeperNativeApi.countDownLatch.await();
+        zookeeperNativeApi.setZooKeeper(zooKeeper);
+
+        // 使用digest的话，就需要使用加密后的密码
+//        String s1 = DigestAuthenticationProvider.generateDigest("123456");
+//        List<ACL> digest = Collections.singletonList(new ACL(ZooDefs.Perms.ALL, new Id("digest", s1)));
+
+        List<ACL> digest = Collections.singletonList(new ACL(ZooDefs.Perms.WRITE, new Id("auth", "1234567")));
+
+        // 需要事先添加exits事件，否则不能监听到节点创建事件
+        zooKeeper.exists("/test", true);
+        // 创建一个临时anyone权限的节点(临时节点不能创建子节点)
+        String s = zooKeeper.create("/test", "haha".getBytes(), digest, EPHEMERAL);
+        System.out.println(s);
+
+        // 开启另外一个客户端，使用不对的密码，查看是否可以查询到数据
+        getDataBadAcl();
+
+        getDataCorrecAcl();
+
+        zooKeeper.close();
+    }
+
+    public static void getDataBadAcl() throws Exception{
+        ZooKeeper zooKeeper = null;
+        try {
+
+            ZookeeperNativeApiForAcl zookeeperNativeApi = new ZookeeperNativeApiForAcl();
+            zooKeeper = new ZooKeeper(SERVER_IP_PORT, 3000, zookeeperNativeApi);
+            // 添加用户
+            zooKeeper.addAuthInfo("digest", "1234567".getBytes());
+            zookeeperNativeApi.countDownLatch.await();
+            byte[] data = zooKeeper.getData("/test", true, null);
+            System.out.println(new String(data));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            zooKeeper.close();
+        }
+
+    }
+
+    public static void getDataCorrecAcl() throws Exception{
+        ZooKeeper zooKeeper = null;
+        try {
+
+            ZookeeperNativeApiForAcl zookeeperNativeApi = new ZookeeperNativeApiForAcl();
+            zooKeeper = new ZooKeeper(SERVER_IP_PORT, 3000, zookeeperNativeApi);
+            // 添加用户
+            zooKeeper.addAuthInfo("digest", "123456".getBytes());
+            zookeeperNativeApi.countDownLatch.await();
+            byte[] data = zooKeeper.getData("/test", true, null);
+            System.out.println(new String(data));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            zooKeeper.close();
+        }
+
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        try {
+
+            Event.KeeperState state = event.getState();
+            Event.EventType type = event.getType();
+            if (state.getIntValue() == Event.KeeperState.SyncConnected.getIntValue()) {
+
+                if (type.getIntValue() == Event.EventType.None.getIntValue()) {
+                    this.countDownLatch.countDown();
+                    System.out.println("连接事件");
+                }
+
+                if (type.getIntValue() == Event.EventType.NodeCreated.getIntValue()) {
+                    String path = event.getPath();
+                    System.out.println(path + ":创建节点事件");
+                    // 再次添加exits事件，否则不能监听到节点创建事件
+                    zooKeeper.exists(path, true);
+            }
+
+                if (type.getIntValue() == Event.EventType.NodeDataChanged.getIntValue()) {
+                    String path = event.getPath();
+                    System.out.println(path + ":节点数据变更事件");
+                    // 由于watch机制是一次性的，在搜到节点变更事件后，需要重新注册数据变更事件
+                    byte[] data = zooKeeper.getData(path, true, null);
+                    System.out.println("变更后的数据:" + new String(data));
+                }
+
+                if (type.getIntValue() == Event.EventType.NodeChildrenChanged.getIntValue()) {
+                    String path = event.getPath();
+                    System.out.println(path + ":子节点变更事件");
+                    zooKeeper.getChildren(path, true);
+                }
+
+                if (type.getIntValue() == Event.EventType.NodeDeleted.getIntValue()) {
+                    String path = event.getPath();
+                    System.out.println("节点删除事件");
+                    // 再次添加exits事件，否则不能监听到节点创建事件
+                    System.out.println("会话id:" + zooKeeper.getSessionId());
+                    zooKeeper.exists(path, true);
+                }
+
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+    }
+}
