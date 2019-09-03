5fe96d9eb558c556c903cc80dab4cccedc7e7678
ypdaic <841943896@qq.com>
2019-09-03 18:24:02 +0800

    Java 基础源码学习

---
+++ cms/src/main/java/com/daiyanping/cms/javabase/HashMapTest.java
@@ -0,0 +1,38 @@
+package com.daiyanping.cms.javabase;
+
+import java.util.HashMap;
+
+/**
+ * @ClassName HashMapTest
+ * @Description TODO
+ * @Author daiyanping
+ * @Date 2019-09-03
+ * @Version 0.1
+ */
+public class HashMapTest {
+
+    public static void main(String[] args) {
+        HashMap<String, Object> stringObjectHashMap = new HashMap<>();
+        stringObjectHashMap.put("1", "sdfdf");
+        stringObjectHashMap.put("2", "sdfdf");
+        stringObjectHashMap.put("3", "sdfdf");
+        stringObjectHashMap.put("4", "sdfdf");
+        stringObjectHashMap.put("5", "sdfdf");
+        stringObjectHashMap.put("6", "sdfdf");
+        stringObjectHashMap.put("7", "sdfdf");
+        stringObjectHashMap.put("8", "sdfdf");
+        stringObjectHashMap.put("9", "sdfdf");
+        stringObjectHashMap.put("10", "sdfdf");
+        stringObjectHashMap.put("11", "sdfdf");
+        stringObjectHashMap.put("12", "sdfdf");
+        stringObjectHashMap.put("13", "sdfdf");
+        stringObjectHashMap.put("14", "sdfdf");
+        stringObjectHashMap.put("15", "sdfdf");
+        stringObjectHashMap.put("16", "sdfdf");
+        stringObjectHashMap.put("18", "sdfdf");
+        stringObjectHashMap.put("19", "sdfdf");
+        stringObjectHashMap.put("20", "sdfdf");
+        stringObjectHashMap.put("21", "sdfdf");
+        stringObjectHashMap.put("22", "sdfdf");
+    }
+}
---
+++ cms/src/main/java/com/daiyanping/cms/javabase/LinkedHashMapTest.java
@@ -0,0 +1,19 @@
+package com.daiyanping.cms.javabase;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @ClassName LinkedHashMapTest
+ * @Description TODO
+ * @Author daiyanping
+ * @Date 2019-09-03
+ * @Version 0.1
+ */
+public class LinkedHashMapTest {
+
+    public static void main(String[] args) {
+        LinkedHashMap<String, String> stringStringLinkedHashMap = new LinkedHashMap<>();
+        stringStringLinkedHashMap.put("sfdsf", "sfsf");
+        stringStringLinkedHashMap.put("sfdsf", "sfssf");
+    }
+}
