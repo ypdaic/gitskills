3beeeb93416948dc4d4488f22af195931ee88ee8
ypdaic <841943896@qq.com>
2019-09-20 18:37:47 +0800

    springcloud学习

--- springcloud/springcloud-security/src/main/java/com/daiyan/springcloud/WebSecurity.java
+++ springcloud/springcloud-security/src/main/java/com/daiyan/springcloud/WebSecurity.java
@@ -13,13 +13,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
-//        http.httpBasic().and().authorizeRequests().anyRequest();
-////                .fullyAuthenticated();
+//        http.httpBasic().and().authorizeRequests().anyRequest()
+//                .fullyAuthenticated();
 //        http.sessionManagement()
 //                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        http.csrf().disable();
+////        http.csrf().disable();
 //        super.configure(http);
 //    }
+
+    /**
+     * 关闭csrf,否则非"GET", "HEAD", "TRACE", "OPTIONS"请求都会被CsrfFilter拦截
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth)
@@ -27,4 +38,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).withUser("root").password(new BCryptPasswordEncoder().encode("enjoy")).roles("USER").
                 and().withUser("admin").password(new BCryptPasswordEncoder().encode("test1234")).roles("USER", "ADMIN");
     }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//
+//        http.httpBasic().and().authorizeRequests().anyRequest()
+//                .fullyAuthenticated();
+//        http.sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//    }
+
 }
