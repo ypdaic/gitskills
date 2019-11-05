1f10d3feff6eab44f3dd93da6a307c883330e534
ypdai <841943896@qq.com>
2019-11-05 23:50:06 +0800

    rocketmq 学习

--- cms/src/main/java/com/daiyanping/cms/rocketmq/nativeapi/retry/SimpleProducer.java
+++ cms/src/main/java/com/daiyanping/cms/rocketmq/nativeapi/retry/SimpleProducer.java
@@ -6,7 +6,7 @@ import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.remoting.common.RemotingHelper;
 
 /**
- * @author 【享学课堂】 King老师
+ *
  * 同步发送
  */
 public class SimpleProducer {
