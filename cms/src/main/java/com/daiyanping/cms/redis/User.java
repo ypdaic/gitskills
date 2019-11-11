f592ee7a636d1ec2848c9cfb99fb929e3236a553
ypdaic <841943896@qq.com>
2019-11-11 18:14:55 +0800

    代码提交

---
+++ cms/src/main/java/com/daiyanping/cms/redis/User.java
@@ -0,0 +1,16 @@
+package com.daiyanping.cms.redis;
+
+import lombok.Data;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author daiyanping
+ * @Date 2019-11-11
+ * @Version 0.1
+ */
+@Data
+public class User {
+
+    private Integer id;
+}
