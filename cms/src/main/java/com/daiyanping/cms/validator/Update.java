8e7141285381939bd8acbcaf6f542c0529b681a0
ypdaic <841943896@qq.com>
2019-12-16 11:58:13 +0800

    自定义校验规则 。

---
+++ cms/src/main/java/com/daiyanping/cms/validator/Update.java
@@ -0,0 +1,13 @@
+package com.daiyanping.cms.validator;
+
+import javax.validation.groups.Default;
+
+/**
+ * @ClassName Update
+ * @Description TODO
+ * @Author daiyanping
+ * @Date 2019-12-16
+ * @Version 0.1
+ */
+public class Update implements Default {
+}
