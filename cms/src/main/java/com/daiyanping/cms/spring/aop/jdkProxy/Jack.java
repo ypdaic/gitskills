6ad75fa9309af5a3475cdfefa505da01db329949
ypdai <841943896@qq.com>
2019-12-25 23:59:18 +0800

    spring 学习

---
+++ cms/src/main/java/com/daiyanping/cms/spring/aop/jdkProxy/Jack.java
@@ -0,0 +1,9 @@
+package com.daiyanping.cms.spring.aop.jdkProxy;
+
+public class Jack implements People {
+    @Override
+    public String eat(String param) {
+        System.out.println("=========Jack老师喜欢吃东西=======");
+        return "=========Jack老师喜欢吃东西=======";
+    }
+}
---
+++ cms/src/main/java/com/daiyanping/cms/spring/aop/jdkProxy/People.java
@@ -0,0 +1,5 @@
+package com.daiyanping.cms.spring.aop.jdkProxy;
+
+public interface People {
+    public String eat(String param);
+}
