4374617102a6a293890dae3932c7ce7fe31ee08e
ypdaic <841943896@qq.com>
2019-12-30 18:54:20 +0800

    代码提交

---
+++ cms/src/main/java/com/daiyanping/cms/STM/Account.java
@@ -0,0 +1,30 @@
+package com.daiyanping.cms.STM;
+
+/**
+ * @ClassName Account
+ * @Description TODO
+ * @Author daiyanping
+ * @Date 2019-12-30
+ * @Version 0.1
+ */
+public class Account {
+
+    //余额
+    private TxnRef<Integer> balance;
+
+    //构造方法
+    public Account(int balance) {
+        this.balance = new TxnRef<Integer>(balance);
+    }
+
+    //转账操作
+    public void transfer(Account target, int amt){
+        STM.atomic( (txn)->{
+            Integer from = balance.getValue(txn);
+            balance.setValue(from-amt, txn);
+            Integer to = target.balance.getValue(txn);
+            target.balance.setValue(to+amt, txn);
+        });
+    }
+
+}
