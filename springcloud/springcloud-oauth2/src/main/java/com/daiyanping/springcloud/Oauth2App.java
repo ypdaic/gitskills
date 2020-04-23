14482e1a3c36e039b6f4449597e2d23db32e9544
daiyanping <841943896@qq.com>
2020-04-24 00:21:32 +0800

    oauth2

--- springcloud/springcloud-oauth2/src/main/java/com/daiyanping/springcloud/Oauth2App.java
+++ springcloud/springcloud-oauth2/src/main/java/com/daiyanping/springcloud/Oauth2App.java
@@ -1,4 +1,16 @@
 package com.daiyanping.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableResourceServer
 public class Oauth2App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2App.class, args);
+    }
 }
