ae37a8169d062ba7450a4b44f4d3e5335997a244
ypdai <841943896@qq.com>
2022-07-05 23:29:29 +0800

    zookeeper 学习。

--- cms/src/main/java/com/daiyanping/cms/zookeeper/CuratorApi.java
+++ cms/src/main/java/com/daiyanping/cms/zookeeper/CuratorApi.java
@@ -3,7 +3,10 @@ package com.daiyanping.cms.zookeeper;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @ClassName CuratorApi
@@ -14,9 +17,32 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
  */
 public class CuratorApi {
 
-    public static void main(String args[]) {
+    static CuratorFramework curatorFramework2;
+
+
+    public static void main(String args[]) throws Exception {
         RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
         CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("127.0.0.1", 3000, 5000, retryPolicy);
+        // 必须调用start
         curatorFramework.start();
+
+        curatorFramework2 = curatorFramework;
+
+        lock();
+    }
+
+    // 分布式锁使用
+    public static void lock() throws Exception {
+
+        InterProcessMutex lock = new InterProcessMutex(curatorFramework2, "/lock_test");
+        if (lock.acquire(10, TimeUnit.SECONDS)) {
+            try {
+                // do some work
+                Thread.sleep(1000);
+                System.out.println("xxxxxxxxxxxx");
+            } finally {
+                lock.release();
+            }
+        }
     }
 }
